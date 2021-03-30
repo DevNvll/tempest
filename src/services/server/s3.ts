46e6f06284eb33d823ece0eabf19f568b53cf559
Henrick Mello <hmartins505@gmail.com>
2021-03-30 20:25:28 -0300

    fix: fix credentials

--- src/services/server/s3.ts
+++ src/services/server/s3.ts
@@ -9,7 +9,11 @@ AWS.config.update({
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   region: 'sa-east-1',
-  signatureVersion: 'v4'
+  signatureVersion: 'v4',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST
+  }
 })
 
 export default s3
