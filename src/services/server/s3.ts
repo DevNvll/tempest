4d4c8b11418adbd04fb3be672a048de9e8ccf9ae
Henrick Mello <hmartins505@gmail.com>
2021-03-30 20:55:02 -0300

    fix: remove credentials from s3 instance

--- src/services/server/s3.ts
+++ src/services/server/s3.ts
@@ -1,4 +1,4 @@
-import * as AWS from 'aws-sdk'
+import AWS from 'aws-sdk'
 import getConfig from 'next/config'
 
 const { serverRuntimeConfig } = getConfig()
@@ -12,11 +12,7 @@ AWS.config.update({
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   region: 'sa-east-1',
-  signatureVersion: 'v4',
-  credentials: {
-    accessKeyId: serverRuntimeConfig.AWS_ACCESS_KEY_ID_TEMPEST,
-    secretAccessKey: serverRuntimeConfig.AWS_SECRET_ACCESS_KEY_TEMPEST
-  }
+  signatureVersion: 'v4'
 })
 
 export default s3
