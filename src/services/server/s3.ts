db68cbc2673ad2cb8b58c3479ef62a73783f5bfa
Henrick Mello <hmartins505@gmail.com>
2021-03-30 20:47:18 -0300

    feat(runtime): add runtime config

--- src/services/server/s3.ts
+++ src/services/server/s3.ts
@@ -1,28 +1,21 @@
 import * as AWS from 'aws-sdk'
+import getConfig from 'next/config'
+
+const { serverRuntimeConfig } = getConfig()
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST,
+  accessKeyId: serverRuntimeConfig.AWS_ACCESS_KEY_ID_TEMPEST,
+  secretAccessKey: serverRuntimeConfig.AWS_SECRET_ACCESS_KEY_TEMPEST,
   region: process.env.AWS_REGION_TEMPEST || 'us-east-1'
 })
 
-console.log({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST,
-  region: process.env.AWS_REGION_TEMPEST || 'us-east-1'
-})
-
-console.log('s3', {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST
-})
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   region: 'sa-east-1',
   signatureVersion: 'v4',
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST
+    accessKeyId: serverRuntimeConfig.AWS_ACCESS_KEY_ID_TEMPEST,
+    secretAccessKey: serverRuntimeConfig.AWS_SECRET_ACCESS_KEY_TEMPEST
   }
 })
 
