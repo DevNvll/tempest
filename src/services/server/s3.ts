06c4c68b6db0b85dd850f65131a261c72ac6010a
Henrick Mello <hmartins505@gmail.com>
2021-03-30 18:34:53 -0300

    feat(aws): add default config

--- src/services/server/s3.ts
+++ src/services/server/s3.ts
@@ -1,5 +1,11 @@
 import * as AWS from 'aws-sdk'
 
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST,
+  region: process.env.AWS_REGION_TEMPEST || 'us-east-1'
+})
+
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   region: 'sa-east-1',
