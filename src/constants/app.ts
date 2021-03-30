6899d823d031aafcb092e12b2e26f01b0b66ce74
Henrick Mello <hmartins505@gmail.com>
2021-03-30 20:03:10 -0300

    fix: fix bucket name env var

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,4 +1,4 @@
-export const BUCKET = process.env.AWS_BUCKET
+export const BUCKET = process.env.AWS_BUCKET_TEMPEST
 export const S3_FILES_PREFIX = process.env.FILES_BUCKET_PREFIX || 'files/'
 export const S3_THUMBNAILS_PREFIX =
   process.env.THUMBNAILS_BUCKET_PREFIX || 'thumbnails/'
