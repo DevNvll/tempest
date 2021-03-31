8cc27de1230efac9b4da0807523874b7fbb11ffd
Henrick Mello <hmartins505@gmail.com>
2021-03-30 21:20:23 -0300

    fix: fix signature version fo aws config

--- src/services/server/s3.ts
+++ src/services/server/s3.ts
@@ -1,15 +1,13 @@
 import AWS from 'aws-sdk'
 
 AWS.config.update({
+  apiVersion: '2006-03-01',
   accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST,
-  region: process.env.AWS_REGION_TEMPEST || 'us-east-1'
-})
-
-const s3 = new AWS.S3({
-  apiVersion: '2006-03-01',
-  region: 'sa-east-1',
+  region: process.env.AWS_REGION_TEMPEST || 'sa-east-1',
   signatureVersion: 'v4'
 })
+
+const s3 = new AWS.S3()
 
 export default s3
