ac0ca7017bd66a7e7ebeace7b77461f6ceff9119
Henrick Mello <hmartins505@gmail.com>
2021-03-30 21:15:19 -0300

    fix: aws sdk config

--- src/services/server/s3.ts
+++ src/services/server/s3.ts
@@ -1,11 +1,15 @@
 import AWS from 'aws-sdk'
 
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST,
+  region: process.env.AWS_REGION_TEMPEST || 'us-east-1'
+})
+
 const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   region: 'sa-east-1',
-  signatureVersion: 'v4',
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID_TEMPEST,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_TEMPEST
+  signatureVersion: 'v4'
 })
 
 export default s3
